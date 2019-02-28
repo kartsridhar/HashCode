1ec4d4928a58f14f5a1f27fed38bf490c1f4f481
kartsridhar <contactkarthik34@gmail.com>
2019-02-28 08:28:57 +0000

    created slice class

--- solution.py
+++ solution.py
@@ -1,14 +1,12 @@
 import numpy as np
 import sys
-from enum import Enum
 
-class Toppings(Enum):
-    T = 0
-    M = 1
 # R (1 ≤ R ≤ 1000) is the number of rows,
 # C (1 ≤ C ≤ 1000) is the number of columns,
 # L (1 ≤ L ≤ 1000) is the minimum number of each ingredient cells in a slice,
 # H (1 ≤ H ≤ 1000) is the maximum total number of cells of a slice
+
+#Tomato = 1 Mushroom = 0
 
 #Reading the input file and returning it as a list
 def read_file(filename = sys.argv[1]):
@@ -22,14 +20,31 @@ def read_file(filename = sys.argv[1]):
     L = vals[2]
     H = vals[3]
     convert = [list(map(lambda t: 1 if t == 'T' else 0, r.strip()))for r in lines[1:]]
-    toppings = np.array(convert)
-    return R, C, L, H, toppings
+    pizza = np.array(convert)
+    return R, C, L, H, pizza
 
-read_file()
-#Each character is either M or T (Mushroom or Tomato)
+R, C, L, H, pizza = read_file()
 
-#Idea 1:
-#choose any random start point that is available
-#enter a random shape size and fit to location chosen
-#check constraints and update the available cells
-#check again, if doesn't exist, stop.
+class Slice(object):
+    def __init__(self, r1, c1, r2, c2):     #Constructor for a slice
+        self.r1 = r1
+        self.c1 = c1
+        self.r2 = r2
+        self.c2 = c2
+
+    def __size__(self):
+        return ((self.r2 - self.r1) + 1) * ((self.c2 - self.c1) + 1)
+
+    def __getTomatoCount__(self):
+        count = 0
+        for i in range(self.r1, self.r2 + 1):
+            for j in range(self.r2, self.c2 + 1):
+                count += pizza[i][j] == 1
+            return count
+
+    def __check__(self):
+        tomato = self.__getTomatoCount__()
+        mushroom = self.__size__() - tomato
+        length = tomato >= L and mushroom >= L
+        height = tomato + mushroom <= H
+        return length and height
